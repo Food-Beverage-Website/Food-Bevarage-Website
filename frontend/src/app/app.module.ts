b75362bde17b180b3adcd9148ee23cdd6d858715
hoakhuu <112261849+hoakhuu@users.noreply.github.com>
2023-10-31 21:10:18 +0700

    initial backend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
 
-import { MatIconModule } from '@angular/material/icon';  
+import { MatIconModule } from '@angular/material/icon';
+import { HomeComponent } from './components/pages/home/home.component';  
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
